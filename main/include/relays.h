b8713b136427ac88a7e20cdbd1079666c8e6d903
akos.sereg <akos.sereg@gmail.com>
2020-12-09 20:04:38 +0100

    refactoring

--- main/include/relays.h
+++ main/include/relays.h
@@ -7,9 +7,9 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define LED_STRIP_LATCH_GPIO	32
-#define LED_STRIP_DATA_GPIO	25
-#define LED_STRIP_CLOCK_GPIO	33
+#define SHIFT_REGISTER_LATCH_GPIO	32
+#define SHIFT_REGISTER_DATA_GPIO	25
+#define SHIFT_REGISTER_CLOCK_GPIO	33
 
 extern void init_relays();
 extern void relays_refresh();
--- main/relays.c
+++ main/relays.c
@@ -1,68 +1,37 @@
+/*
+  Relays are connected to a SN74HC595 shift register, which is driven by ESP32.
+*/
+
 #include "include/relays.h"
 #include "include/state.h"
 
 void init_relays() {
     // init shift register gpios
-    gpio_set_direction(LED_STRIP_LATCH_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_STRIP_CLOCK_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_direction(LED_STRIP_DATA_GPIO, GPIO_MODE_OUTPUT);
-
-    // init red led' gpio
-    // gpio_set_direction(LED_STRIP_RED, GPIO_MODE_OUTPUT);
-    // gpio_pad_select_gpio(LED_STRIP_RED);
+    gpio_set_direction(SHIFT_REGISTER_LATCH_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(SHIFT_REGISTER_CLOCK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(SHIFT_REGISTER_DATA_GPIO, GPIO_MODE_OUTPUT);
 }
 
 
 void relays_refresh() {
 
     for (int i=0; i!=8; i++) {
-        gpio_set_level(LED_STRIP_CLOCK_GPIO, 0);
+        gpio_set_level(SHIFT_REGISTER_CLOCK_GPIO, 0);
 
         switch(i) {
-            // computer (in)
-            case 7:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.relay_in_comp);
-                break;
-
-            // guitar (in)
-            case 6:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.relay_in_guitar);
-                break;
-
-            // vinyl (in)
-            case 5:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.relay_in_vinyl);
-                break;
-
-            // headphones (out)
-            case 4:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_headphones);
-                break;
-
-            // speaker (out)
-            case 3:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_speaker);
-                break;
-
-            // amplifier (out)
-            case 2:
-                gpio_set_level(LED_STRIP_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_amplifier);
-                break;
-
-            // dummy
-            case 1:
-                gpio_set_level(LED_STRIP_DATA_GPIO, 0);
-                break;
-
-            // dummy
-            case 0:
-                gpio_set_level(LED_STRIP_DATA_GPIO, 0);
-                break;
+            case 0: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, 0); break;
+            case 1: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, 0); break;
+            case 2: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_amplifier); break;
+            case 3: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_speaker); break;
+            case 4: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.is_muted ? 0 : app_state.relay_out_headphones); break;
+            case 5: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.relay_in_vinyl); break;
+            case 6: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.relay_in_guitar); break;
+            case 7: gpio_set_level(SHIFT_REGISTER_DATA_GPIO, app_state.relay_in_comp); break;
         }
 
-	    gpio_set_level(LED_STRIP_CLOCK_GPIO, 1);
+	    gpio_set_level(SHIFT_REGISTER_CLOCK_GPIO, 1);
     }
 
-    gpio_set_level(LED_STRIP_LATCH_GPIO, 1);
-    gpio_set_level(LED_STRIP_LATCH_GPIO, 0);
+    gpio_set_level(SHIFT_REGISTER_LATCH_GPIO, 1);
+    gpio_set_level(SHIFT_REGISTER_LATCH_GPIO, 0);
 }
